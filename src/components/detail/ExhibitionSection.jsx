b0ca1ba3897be85b48215bbc52120035b93421b6
Ray Lee <ray.lee@lyrasis.org>
2021-04-23 20:36:15 -0400

    DRYD-931: Convert text to intl messages.

--- src/components/detail/ExhibitionSection.jsx
+++ src/components/detail/ExhibitionSection.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { defineMessages, FormattedMessage } from 'react-intl';
 import styles from '../../../styles/cspace/Exhibition.css';
 
 const propTypes = {
@@ -15,6 +16,21 @@ const defaultProps = {
   ownerConrtibutionNote: undefined,
   viewerContributionNote: undefined,
 };
+
+const messages = defineMessages({
+  objectStoryHeader: {
+    id: 'exhibitionSection.objectStoryHeader',
+    defaultMessage: 'Object Story',
+  },
+  donorStoryHeader: {
+    id: 'exhibitionSection.donorStoryHeader',
+    defaultMessage: 'Donor Story',
+  },
+  curatorialStoryHeader: {
+    id: 'exhibitionSection.curatorialStoryHeader',
+    defaultMessage: 'Curatorial Story',
+  },
+});
 
 export default class ExhibitionSection extends Component {
   constructor() {
@@ -36,12 +52,15 @@ export default class ExhibitionSection extends Component {
         <hr />
         <h1>{exhibition[0].title}</h1>
         <div />
-        <h2>Object Story</h2>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <h2><FormattedMessage {...messages.objectStoryHeader} /></h2>
         <div>{historyNote}</div>
-        <h2>Donor Story</h2>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <h2><FormattedMessage {...messages.donorStoryHeader} /></h2>
         <div>{ownerConrtibutionNote}</div>
         <div>{exhibition[0].generalNote}</div>
-        <h2>Curatorial Story</h2>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <h2><FormattedMessage {...messages.curatorialStoryHeader} /></h2>
         <div>{viewerContributionNote}</div>
         <div>{exhibition[0].curatorialNote}</div>
       </>

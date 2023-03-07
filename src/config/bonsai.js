4dd4216657c1206981ce7617674db71c9fd14e4b
Ray Lee <ray.lee@lyrasis.org>
2023-03-07 14:03:02 -0500

    CB-1: Add config overrides for bonsai profile.

--- src/config/bonsai.js
+++ src/config/bonsai.js
@@ -1,3 +1,50 @@
+import { defineMessages } from 'react-intl';
+
+import {
+  valueAt,
+} from '../helpers/formatHelpers';
+
 export default {
   gatewayUrl: 'http://localhost:8180/gateway/bonsai',
+  messages: {
+    'filter.objectProductionPerson.label': 'Original artist',
+    'filter.objectProductionPerson.shortLabel': 'Original artist',
+    'detailField.objectProductionPerson.label': 'Original artist',
+  },
+  detailFields: {
+    fields: {
+      originDate: {
+        messages: defineMessages({
+          label: {
+            id: 'detailField.originDate.label',
+            defaultMessage: 'Date of origin',
+          },
+        }),
+        field: 'collectionobjects_common:objectProductionDateGroupList',
+        format: valueAt({ path: [0, 'dateDisplayDate'] }),
+      },
+      trainingDate: {
+        messages: defineMessages({
+          label: {
+            id: 'detailField.trainingDate.label',
+            defaultMessage: 'In training since',
+          },
+        }),
+        field: 'collectionobjects_common:objectProductionDateGroupList',
+        format: valueAt({ path: [1, 'dateDisplayDate'] }),
+      },
+    },
+    groups: {
+      group_production: {
+        fields: [
+          'objectProductionPerson',
+          'objectProductionOrganization',
+          'objectProductionPeople',
+          'objectProductionPlace',
+          'originDate',
+          'trainingDate',
+        ],
+      },
+    },
+  },
 };

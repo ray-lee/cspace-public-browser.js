a8c5ea21f96df38a697a5272ad4c8b84b3ec14bc
Michael Ritter <mikejritter@users.noreply.github.com>
2024-03-06 19:48:09 -0700

    CB-29: Use deurned field for objectProductionPlace (#31)

--- src/config/lhmc.js
+++ src/config/lhmc.js
@@ -1,3 +1,11 @@
 export default {
   gatewayUrl: 'http://localhost:8180/gateway/lhmc',
+
+  filters: {
+    fields: {
+      objectProductionPlace: {
+        field: 'collectionobjects_common:objectProductionPlaceGroupList.objectProductionPlace.displayName',
+      },
+    },
+  },
 };
